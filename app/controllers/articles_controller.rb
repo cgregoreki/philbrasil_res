22f39699e4cc1868180afde2f7701dc6ca112ead
Carlos Gregoreki <cgregoreki@gmail.com>
2016-11-01 23:32:09 -0200

    adding images for about page. forbidding access to edit and destroy pages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,9 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+    render status: :forbidden, text: "You do not have access to this page."
+
+
   end
 
   # POST /articles
@@ -40,25 +43,31 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+
+    render status: :forbidden, text: "You do not have access to this page."
+
+    # respond_to do |format|
+    #   if @article.update(article_params)
+    #     format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @article }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render status: :forbidden, text: "You do not have access to this page."
+
+    # @article.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
+
   end
 
 

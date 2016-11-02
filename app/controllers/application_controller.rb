0d3d4c44de1537eee4a62fdca9279d972a44ca9e
Carlos Gregoreki <cgregoreki@gmail.com>
2016-11-02 21:53:54 -0200

    highlighting buttons on menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-
+    @active_menu = "home"
   end
 
   def about
-
+    @active_menu = "about"
   end
 
   def collaborate
+    @active_menu = "collaborate"
   end
 
   def updates
+    @active_menu = "updates"
   	@last_submissions = Article.all.order(created_at: :desc).limit(50)
   end
   

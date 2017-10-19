a434113b2e9a01e8596e2fd9af0746fe4af1558a
C.Gregoreki <cgregoreki@gmail.com>
2017-10-19 17:54:05 -0200

    generated sessions and other controllers from devise. added simple independent layout for sign_in page. configured routes for devise on users. session controller from devise has layout: false.

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,33 @@
 Rails.application.routes.draw do
 
-    
-  	devise_for :users
-  	
-	resources :articles do
-	 	post 'access'
-	
-	 	post 'report'
-	 	post 'report_submit'
-	end
+  # devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'user/sessions'
+  }
 
-	root to: 'application#home'
-	authenticate :user do
-		get 'dashboard', to: "dashboard#index"
-	end
-	
-	resources :categories, only: [:index, :show]
-	
-	get 'search', to: "articles#search"
-	get 'sobre', to: "application#about"
-	get 'colabore', to: "application#collaborate"
-	get 'atualizacoes', to: "application#updates"
-	get 'advanced-search', to: "application#advanced_search"
-	get 'magazines', to: "application#magazines"
-	get 'blog', to: "application#blog"
-	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    post 'access'
+    post 'report'
+    post 'report_submit'
+  end
 
-	get 'contact', to: 'contacts#new', as: 'contact' # contact_path returns contacts.new
-	post 'contact', to: 'contacts#create'
+  root to: 'application#home'
+  authenticate :user do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
+  resources :categories, only: [:index, :show]
+
+  get 'search', to: 'articles#search'
+  get 'sobre', to: 'application#about'
+  get 'colabore', to: 'application#collaborate'
+  get 'atualizacoes', to: 'application#updates'
+  get 'advanced-search', to: 'application#advanced_search'
+  get 'magazines', to: 'application#magazines'
+  get 'blog', to: 'application#blog'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'contact', to: 'contacts#new', as: 'contact' # contact_path returns contacts.new
+  post 'contact', to: 'contacts#create'
 end
